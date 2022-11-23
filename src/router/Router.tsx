359942838711ea5d9b0ed610b88108f3cc35dc92
yoshimai <117501051+yoshimai@users.noreply.github.com>
2022-11-23 16:13:22 +0900

    page404追加

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,12 +1,22 @@
 import { memo, VFC } from "react";
 import { Routes, Route } from "react-router-dom";
+import { Home } from "../components/pages/Home";
 import { Login } from "../components/pages/Login";
-import {} from "./HomeRoutes";
+import { Setting } from "../components/pages/Setting";
+import { UserManegement } from "../components/pages/UserManagement";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router: VFC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="Home">
+        <Route path="/Home" element={<Home />} />
+        <Route path="UserManegement" element={<UserManegement />} />
+        <Route path="Setting" element={<Setting />} />
+        <Route path="*" element={<Page404 />} />
+      </Route>
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 });
